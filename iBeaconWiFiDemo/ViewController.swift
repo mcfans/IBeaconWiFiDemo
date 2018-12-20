6f899ed3e549e8e1a2f81c347ddbde1707177e48
mcfans <mcfans@icloud.com>
2018-12-20 19:03:54 +0800

    Initial Commit

---
+++ iBeaconWiFiDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iBeaconWiFiDemo
+//
+//  Created by 杨学思 on 2018/12/20.
+//  Copyright © 2018 杨学思. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
