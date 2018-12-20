ee83723e3910bdcdb58b886c3272dc2a6f2d4081
mcfans <mcfans@icloud.com>
2018-12-20 19:13:28 +0800

    first commit

--- iBeaconWiFiDemo/ViewController.swift
+++ iBeaconWiFiDemo/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    let beaconRegion = CLBeaconRegion(proximityUUID: UUID(), identifier: "hhhh")
+    
+    var task: URLSessionDataTask?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.startRangingBeacons(in: beaconRegion)
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let request = URLRequest(url: URL(string: "https://www.baidu.com")!)
+        task = URLSession.shared.dataTask(with: request) { (data, res, error) in
+            print(data, res, error)
+        }
+    }
+    
+    var firstTime: Bool = true
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if firstTime {
+            task?.resume()
+            firstTime.toggle()
+        }
+        print("ranging beacon")
     }
 
 
